4dd13dc5b5aa6e80847dbf09c73dcfcb1aa34839
Kshitiz <dikhwachha@gmail.com>
2025-04-25 18:15:05 +0545

    working contact creation

--- src/infrastructure/database-service/database-service.module.ts
+++ src/infrastructure/database-service/database-service.module.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 
 import { Intake } from '@domain/entities/intake.entity';
 import { IntakeStage } from '@domain/entities/intake-stage.entity';
+import { Contact } from '@domain/entities/contact.entity';
 
 import { DatabaseService } from '@domain/abstractions/integration-services';
 
@@ -17,21 +18,18 @@ import { IntakeEvent } from '@domain/entities/intake-event.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        synchronize: true,
-        autoLoadEntities: true,
-        entities: [path.resolve(__dirname, '../../domain/entities/*.{js,ts}')]
-      }),
-      inject: [ConfigService]
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      synchronize: true,
+      autoLoadEntities: true,
+      entities: [path.resolve(__dirname, '../../domain/entities/*.{js,ts}')]
     }),
-    TypeOrmModule.forFeature([Intake, IntakeStage, IntakeEvent])
+    TypeOrmModule.forFeature([Intake, IntakeStage, IntakeEvent, Contact])
   ],
   providers: [
     {
